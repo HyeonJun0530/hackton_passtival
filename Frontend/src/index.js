ca0771aba4acbeb7c179d99a59a7464958e0d9d4
pp449 <mma7710@naver.com>
2022-08-13 16:29:04 +0900

    set global recoil and set folder structure

--- Frontend/src/index.js
+++ Frontend/src/index.js
@@ -1,14 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { RecoilRoot } from 'recoil';
 import './index.css';
 import App from './App';
+import { BrowserRouter as Router } from 'react-router-dom';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <Router>
+    <RecoilRoot>
+      <App />
+    </RecoilRoot>
+  </Router>
 );
 
 // If you want to start measuring performance in your app, pass a function
