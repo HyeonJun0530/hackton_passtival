c7c0837a29faad00c2d6c08e44335bea77dc3872
pepe6bf <pepe6bf@gmail.com>
2022-08-13 15:24:25 +0900

    Backend 프로젝트 업로드

---
+++ Backend/passtival/src/test/java/com/passtival/passtival/PasstivalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.passtival.passtival;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PasstivalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
